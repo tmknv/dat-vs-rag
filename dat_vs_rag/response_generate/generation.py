30678383a5812cc1973799c9fb582e8702dcdb28
Xchanskiy <loonix166@gmail.com>
2026-04-07 14:12:22 +0300

    change response structur

--- dat_vs_rag/response_generate/generation.py
+++ dat_vs_rag/response_generate/generation.py
@@ -8,7 +8,7 @@ def get_responses(query: str, RAG_retriever_type: str, alpha_coefficient: float)
     DAT_response = DAT_SLM_response(processed_query)
     RAG_response = RAG_LLM_response(processed_query, RAG_retriever_type, alpha_coefficient)
 
-    total_response = f"DAT SLM response:\n{DAT_response}\nRAG LLM response:\n{RAG_response}"
+    total_response = f"DAT SLM response:\n{DAT_response}\n{RAG_retriever_type} RAG LLM response:\n{RAG_response}"
 
     return total_response
     
